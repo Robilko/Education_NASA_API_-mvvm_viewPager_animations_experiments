12285b4c5872e31eec67671d93c742b70c21f935
Alexander <75025794+Robilko@users.noreply.github.com>
2021-10-21 00:01:54 +0300

    пункт 2. Создан первый экран приложения с фотографией дня.

--- app/src/main/java/com/example/photoeveryday/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/photoeveryday/ui/main/view/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.example.photoeveryday
+package com.example.photoeveryday.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.photoeveryday.ui.main.MainFragment
+import com.example.photoeveryday.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, PictureOfTheDayFragment())
                 .commitNow()
         }
     }
---
+++ app/src/main/java/com/example/photoeveryday/ui/main/view/PictureOfTheDayFragment.kt
@@ -0,0 +1,74 @@
+package com.example.photoeveryday.ui.main.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import coil.api.load
+import com.example.photoeveryday.R
+import com.example.photoeveryday.databinding.MainFragmentBinding
+import com.example.photoeveryday.ui.main.repository.PictureOfTheDayData
+import com.example.photoeveryday.ui.main.utils.showSnackBar
+import com.example.photoeveryday.ui.main.viewmodel.PictureOfTheDayViewModel
+
+class PictureOfTheDayFragment : Fragment() {
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    //Ленивая инициализация модели
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getData().observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    //Отобразите ошибку
+                    // showError("Сообщение, что ссылка пустая")
+                    binding.main.showSnackBar(
+                        getString(R.string.error_message_empty_url),
+                        getString(R.string.reload),
+                        { View.OnClickListener() { _ -> } }
+                    )
+                } else {
+                    //Отобразите фото
+                    // showSuccess()
+                    // Coil в работе: достаточно вызвать у нашего ImageView
+                    // нужную extension-функцию и передать ссылку и заглушки для placeholder
+                    binding.imageView.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                //Отобразите загрузку
+                // showLoading()
+            }
+            is PictureOfTheDayData.Error -> {
+                //Отобразите ошибку
+            }
+        }
+    }
+}
