f2c9120d3b13777870c6d34e14fb530226780e4d
Alexander <75025794+Robilko@users.noreply.github.com>
2021-10-22 10:43:28 +0300

    пункт 3. Добавлено описание (приходит с сервера) под фотографией в виде BottomSheet.

--- app/src/main/java/com/example/photoeveryday/ui/main/view/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/photoeveryday/ui/main/view/PictureOfTheDayFragment.kt
@@ -6,19 +6,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.api.load
 import com.example.photoeveryday.R
 import com.example.photoeveryday.databinding.MainFragmentBinding
+import com.example.photoeveryday.ui.main.repository.PODServerResponseData
 import com.example.photoeveryday.ui.main.repository.PictureOfTheDayData
 import com.example.photoeveryday.ui.main.utils.showSnackBar
 import com.example.photoeveryday.ui.main.viewmodel.PictureOfTheDayViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.bottom_sheet_container.*
 
 class PictureOfTheDayFragment : Fragment() {
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     //Ленивая инициализация модели
     private val viewModel: PictureOfTheDayViewModel by lazy {
@@ -37,6 +42,7 @@ class PictureOfTheDayFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getData().observe(viewLifecycleOwner, { renderData(it) })
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
         binding.inputLayout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
                 data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
@@ -44,30 +50,18 @@ class PictureOfTheDayFragment : Fragment() {
         }
     }
 
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
     private fun renderData(data: PictureOfTheDayData) {
         when (data) {
             is PictureOfTheDayData.Success -> {
                 val serverResponseData = data.serverResponseData
-                val url = serverResponseData.url
-                if (url.isNullOrEmpty()) {
-                    //Отобразите ошибку
-                    // showError("Сообщение, что ссылка пустая")
-                    binding.main.showSnackBar(
-                        getString(R.string.error_message_empty_url),
-                        getString(R.string.reload),
-                        { View.OnClickListener { } }
-                    )
-                } else {
-                    //Отобразите фото
-                    // showSuccess()
-                    // Coil в работе: достаточно вызвать у нашего ImageView
-                    // нужную extension-функцию и передать ссылку и заглушки для placeholder
-                    binding.imageView.load(url) {
-                        lifecycle(this@PictureOfTheDayFragment)
-                        error(R.drawable.ic_load_error_vector)
-                        placeholder(R.drawable.ic_no_photo_vector)
-                    }
-                }
+                serverResponseData.url?.let {
+                    showSuccess(serverResponseData)
+                } ?: showError(getString(R.string.error_message_empty_url))
             }
             is PictureOfTheDayData.Loading -> {
                 //Отобразите загрузку
@@ -78,4 +72,24 @@ class PictureOfTheDayFragment : Fragment() {
             }
         }
     }
+
+    private fun showSuccess(data: PODServerResponseData) {
+        binding.imageView.load(data.url) {
+            lifecycle(this@PictureOfTheDayFragment)
+            error(R.drawable.ic_load_error_vector)
+            placeholder(R.drawable.ic_no_photo_vector)
+        }
+        view?.findViewById<TextView>(R.id.bottom_sheet_description_header)?.text = data.title
+        binding.imageView.contentDescription = data.title
+        view?.findViewById<TextView>(R.id.bottom_sheet_description)?.text = data.explanation
+    }
+
+    private fun showError(error: String) {
+        binding.main.showSnackBar(
+            error,
+            getString(R.string.reload),
+            { View.OnClickListener { } }
+        )
+    }
+
 }
