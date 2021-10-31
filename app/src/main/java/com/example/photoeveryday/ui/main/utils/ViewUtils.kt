255813e06a6ee67af0d1d1991fd05506459015c9
Robilko <sasha.robson@gmail.com>
2021-10-31 14:40:21 +0300

    Урок 2. 1. Создайте стили для ваших компонентов и применяйте эти стили + 2. Создайте две темы для вашего приложения и в отдельном экране настроек добавьте возможность менять эти темы. + 3. Создайте свой уникальный стиль для приложения и его виджетов. + 4. * Используйте свои шрифты в приложении. + Шрифты добавил, и применил в темах, но не знаю как менять  в рантайме на все приложение 5. * Примените автоматическую тёмную тему для девайсов на 10+ Android. +работает

--- app/src/main/java/com/example/photoeveryday/ui/main/utils/ViewUtils.kt
+++ app/src/main/java/com/example/photoeveryday/ui/main/utils/ViewUtils.kt
@@ -3,6 +3,13 @@ package com.example.photoeveryday.ui.main.utils
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
+const val SETTINGS_PREFERENCES = "SETTINGS_PREFERENCES"
+const val THEME_PREFERENCES = "THEME_PREFERENCES"
+const val KEY_DEFAULT_THEME = "DEFAULT_THEME"
+const val KEY_COSMIC_THEME = "COSMIC_THEME"
+const val CHIP_THEME_SELECTED = "CHIP_THEME_SELECTED"
+const val APP_FONT_FAMILY = "APP_FONT_FAMILY"
+
 fun View.showSnackBar(
     text: String,
     actionText: String,
--- app/src/main/java/com/example/photoeveryday/ui/main/view/SettingsFragment.kt
+++ app/src/main/java/com/example/photoeveryday/ui/main/view/SettingsFragment.kt
@@ -1,14 +1,14 @@
 package com.example.photoeveryday.ui.main.view
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.photoeveryday.R
 import com.example.photoeveryday.databinding.FragmentSettingsBinding
-import com.example.photoeveryday.databinding.MainFragmentBinding
+import com.example.photoeveryday.ui.main.utils.*
 import com.google.android.material.chip.Chip
 
 class SettingsFragment : Fragment() {
@@ -23,18 +23,42 @@ class SettingsFragment : Fragment() {
         return binding.root
     }
 
+    private fun loadSettings() {
+        with(binding) {
+            val checkedId =
+                requireActivity().getSharedPreferences(SETTINGS_PREFERENCES, Context.MODE_PRIVATE).getInt(
+                    CHIP_THEME_SELECTED, defaultTheme.id
+                )
+            chipGroupThemeSelection.findViewById<Chip>(checkedId).isChecked = true
+        }
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.chipGroupThemeSelection.setOnCheckedChangeListener{chipGroup, position ->
-            chipGroup.findViewById<Chip>(position)?.let {
-                Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+        loadSettings()
+        with(binding) {
+            chipGroupThemeSelection.setOnCheckedChangeListener { chipGroup, checkedId ->
+                when (chipGroup.findViewById<Chip>(checkedId)) {
+                    defaultTheme -> saveThemeMode(KEY_DEFAULT_THEME, checkedId)
+                    cosmicTheme -> saveThemeMode(KEY_COSMIC_THEME, checkedId)
+
+                }
             }
-        }
-        binding.chipGroupFontSelection.setOnCheckedChangeListener{chipGroup, position ->
-            chipGroup.findViewById<Chip>(position)?.let {
-                Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+            chipGroupFontSelection.setOnCheckedChangeListener { chipGroup, checkedId ->
+                chipGroup.findViewById<Chip>(checkedId)?.let {
+                    Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
 
+    private fun saveThemeMode(theme: String, checkedId: Int) {
+        requireContext().getSharedPreferences(SETTINGS_PREFERENCES, Context.MODE_PRIVATE)
+            .edit()
+            .putString(THEME_PREFERENCES, theme)
+            .putInt(CHIP_THEME_SELECTED, checkedId)
+            .apply()
+        requireActivity().recreate()
+    }
 }
